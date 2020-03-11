f59ec0873e2809bae84bb5eb25da1a532a42b466
rodvind <rod.danaei@gmail.com>
2020-03-11 16:18:59 -0600

    Add custom argument to my pipe

--- src/app/conv-mile-to-km.pipe.ts
+++ src/app/conv-mile-to-km.pipe.ts
@@ -5,8 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvMileToKMPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return value ? value * 1.60934 : 0;
+  transform(value: any, unit: string): any {
+    if (!value) {
+      return 0;
+    }
+    switch (unit) {
+      case 'km':
+        return value * 1.60934;
+      case 'm':
+        return value * 1.60934 * 1000;
+      default:
+        throw new Error('Target unit not supported!');
+    }
+    // return value ? value * 1.60934 : 0;
   }
 
 }
